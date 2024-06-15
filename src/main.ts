7c9858cf20be258afc5a5a55027d2f973869c5ae
pettmatt <petterimattila@protonmail.com>
2024-06-15 13:15:19 +0300

    store added using pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import "./assets/main.css"
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+createApp(app).mount("#app")
---
+++ src/stores/hud-store.ts
@@ -0,0 +1,74 @@
+import { defineStore } from "pinia"
+
+export const useSessionStore = defineStore("session", {
+    state: () => ({
+        // Booleans
+        settingUpSessionStatus: false,
+        sessionStartStatus: false,
+        placeMarkersStatus: false,
+        highscoresStatus: false,
+        historyStatus: false,
+
+        // numeric
+        traveledDistance: 0,
+        plannedLength: 0,
+        routeLength: 0,
+
+        // Functions
+        removeMarkersFunction: undefined,
+        createRouteFunction: undefined,
+        createLoopFunction: undefined,
+        removeLastMarker: undefined,
+        getMapStatistics: undefined,
+
+        // User session settings
+        // These values are meant to be stored and displayed in History / Highscore menu
+        sessionMarkers: undefined,
+        goal: {
+            category: undefined,
+            goal: {
+                markers: undefined,
+                estimatedDistance: undefined,
+                lengthSpecified: false,
+                distance: undefined,
+                time: undefined,
+            },
+            session: {
+                routeCoordinates: undefined,
+                markerCount: undefined,
+                passedThroughMarkers: undefined,
+                distance: undefined,
+                startingTime: undefined,
+                endTimer: undefined
+            }
+        }
+    })
+})
+
+export const useSettingsStore = defineStore("settings", {
+    state: () => ({
+        settingsStatus: false,
+        settings: {
+            menus: {
+                display: {
+                    // fadeTopOnIdle: getItemFromLocalStorage("fadeTopOnIdle"),
+                    // fadeRightOnIdle: getItemFromLocalStorage("fadeRightOnIdle"),
+                    // fadeBottomOnIdle: getItemFromLocalStorage("fadeBottomOnIdle"),
+                },
+                functionality: {
+                    // enableIdle: getItemFromLocalStorage("fadeBottomOnIdle"),
+                    // idconstimer: getItemFromLocalStorage("idconstimer") || 10
+                }
+            },
+            appFunctionality: {
+                general: {
+                    // offlineMode: getItemFromLocalStorage("offlineMode"),
+                    units: { name: "metric", unit: "m"} || { name: "imperial", unit: ""}
+                },
+                services: {
+                    // allowThirdPartyServices: getItemFromLocalStorage("allowThirdPartyServices")
+                }
+            }
+        }
+    })
+})
