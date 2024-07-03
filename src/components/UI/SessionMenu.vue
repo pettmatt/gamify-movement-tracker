2bc83b12cb77bc98c21777e41c916a82792a9679
pettmatt <petterimattila@protonmail.com>
2024-07-03 16:34:14 +0300

    Created session menu component

---
+++ src/components/UI/SessionMenu.vue
@@ -0,0 +1,32 @@
+<template>
+<h2>Session menu</h2>
+
+<p>Progress: {{ distances.current }} km</p>
+<p v-show="distances.goal">Your goal: </p>
+
+<button @click="endSession">End the session</button>
+</template>
+
+<script setup lang="ts">
+import { sessionStore, sessionDetails } from "@/stores/hud-store"
+import { ref, watch } from "vue"
+
+const distances = ref<object>({
+    current: null,
+    goal: sessionDetails.goal.distance
+})
+
+function createPrompt() {}
+
+function endSession() {
+    sessionStore.sessionStartStatus = false
+    // const sessionStatistics = getMapStatistics()
+}
+
+watch(() => sessionStore.traveledDistance, (currentDistance) => {
+    distances.value.current = currentDistance
+})
+</script>
+
+<style>
+</style>
