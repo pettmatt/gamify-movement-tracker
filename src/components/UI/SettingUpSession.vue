2cc6da6f8b8cf398637df504360be7dfe801d43e
pettmatt <petterimattila@protonmail.com>
2024-07-03 15:50:58 +0300

    Created setting-up session component

---
+++ src/components/UI/SettingUpSession.vue
@@ -0,0 +1,56 @@
+<template>
+<h2>Setting up session</h2>
+
+<div>Sport category 
+    <select name="SessionCategory" :onchange="handleSessionCategory" :value="sessionCategory">
+        <option value="running">Running</option>
+        <option value="walking">Walking</option>
+        <option value="cycling">Cycling</option>
+        <option value="other">Other</option>
+    </select>
+</div>
+<div>
+    Plan your route <button @click="planSessionRoute">Plan</button>
+</div>
+<div>
+    Specify session length goals <input type="checkbox" name="SpecifySessionLength" @click="toggleSpecifySessionLength" value="lengthSpecified" />
+</div>
+
+<div v-show="specifySessionLength">
+    <div>Session time goal <input type="number" name="GoalTime" :value="goalTime" /> mins</div>
+    <div>Session distance goal <input type="number" name="GoalDistance" :value="goalDistance" /> km</div>
+</div>
+
+<button @click="startSession">Start the session</button>
+</template>
+
+<script setup lang="ts">
+import { sessionDetails, sessionStore } from "@/stores/hud-store"
+import { ref } from "vue"
+
+const specifySessionLength = ref(false)
+const goalTime = ref(20)
+const goalDistance = ref(2.5)
+const sessionCategory = ref(undefined)
+
+function toggleSpecifySessionLength() {
+    specifySessionLength.value = !specifySessionLength.value
+}
+function planSessionRoute() {
+    sessionStore.placeMarkersStatus = !sessionStore.placeMarkersStatus
+}
+function handleSessionCategory(e: any) {
+    console.log("handleSessionCategory event value", e)
+    sessionCategory.value = e.value
+}
+
+function startSession() {
+    sessionDetails.category = sessionCategory.value
+    sessionDetails.goal.lengthSpecified = specifySessionLength.value
+
+    if (specifySessionLength.value) {
+        sessionDetails.goal.distance = goalDistance.value
+        sessionDetails.goal.time = goalTime.value
+    }
+}
+</script>
