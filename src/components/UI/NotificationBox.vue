bf934028d85e6d39c05c0f2168d14e9f7d6f3e28
pettmatt <petterimattila@protonmail.com>
2024-07-03 16:26:40 +0300

    Created Notifaction box component

---
+++ src/components/UI/NotificationBox.vue
@@ -0,0 +1,37 @@
+<template>
+<main>
+    <div id="notification-box" :class="props?.position">
+        <slot></slot>
+    </div>
+</main>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    position: String
+})
+</script>
+
+<style>
+#notification-box {
+    border-radius: 0.5em;
+    display: flex;
+    flex-direction: column;
+    padding: 0.5em 1em;
+    max-width: 700px;
+    max-height: 100%;
+    overflow-y: scroll;
+    color: #000;
+    background: linear-gradient(145deg, rgba(255, 255, 255, 0.8), rgba(144, 144, 144, 0.4));
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+}
+#notification-box {
+    pointer-events: all;
+}
+.default {
+    margin: auto;
+}
+.top {
+    margin: 0 auto auto auto;
+}
+</style>
