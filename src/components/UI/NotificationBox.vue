f0712d6103e05b4633d89c37a4b6ff9828d8cd45
pettmatt <petterimattila@protonmail.com>
2024-07-12 15:24:13 +0300

    Tweaking css

--- src/components/UI/NotificationBox.vue
+++ src/components/UI/NotificationBox.vue
@@ -10,14 +10,14 @@ const props = defineProps({
 })
 </script>
 
-<style scoped>
+<style>
 #notification-box {
     border-radius: 0.5em;
     display: flex;
     flex-direction: column;
     padding: 0.5em 1em;
-    max-width: 50em;
-    max-height: 100%;
+    max-width: 40em;
+    max-height: 80vh;
     overflow-y: scroll;
     color: #000;
     background: linear-gradient(145deg, rgba(255, 255, 255, 0.8), rgba(144, 144, 144, 0.4));
@@ -26,6 +26,14 @@ const props = defineProps({
 #notification-box {
     pointer-events: all;
 }
+#notification-box > div {
+    margin-top: 0em;
+    margin-bottom: 0.3em;
+}
+#notification-box p {
+    margin: 0;
+    margin-bottom: 0.3em;
+}
 .default {
     margin: auto;
 }
@@ -33,5 +41,17 @@ const props = defineProps({
     border-top-left-radius: 0 !important;
     border-top-right-radius: 0 !important;
     margin: 0 auto auto auto;
+}
+.bottom {
+    margin: auto;
+    margin-bottom: 0.5em;
+    width: 80vw;
+}
+
+@media only screen and (max-width: 435px) {
+    #notification-box {
+        margin-right: 1em;
+        margin-left: 1em;
+    }
 }
 </style>
