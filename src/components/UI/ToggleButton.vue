05a25f73335dddd00afcc2cff3a0da6461a7c6d5
pettmatt <petterimattila@protonmail.com>
2024-07-12 15:25:14 +0300

    Tweaking how text is shown

--- src/components/UI/ToggleButton.vue
+++ src/components/UI/ToggleButton.vue
@@ -1,12 +1,16 @@
 <template>
-<button :id="`button-${label.toLowerCase()}`" :class="{ highlight: value }" @click="props.storeUpdateFunction">
-    <component :is="currentIcon" class="icon" />
-    <small>{{ label }}</small>
-</button>
+<div class="toggle-button-container">
+    <div class="hover-element" v-show="isHovering">
+        <small>{{ label }}</small>
+    </div>
+    <button :id="`button-${label.toLowerCase()}`" :class="{ highlight: value }" @click="props.storeUpdateFunction" @mouseenter="handleMouseOver(true)" @mouseleave="handleMouseOver(false)">
+        <component :is="currentIcon" class="icon" />
+    </button>
+</div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from "vue"
+import { computed, ref, watch } from "vue"
 
 const props = defineProps({
     label: {
@@ -27,10 +31,24 @@ const props = defineProps({
     }
 })
 
+const isHovering = ref<boolean>(false)
 const currentIcon = computed(() => (props.value ? props.icons.checked : props.icons.default))
+
+function handleMouseOver(value: boolean) {
+    if (!props.value)
+        isHovering.value = value
+}
+
+watch(() => props.value, (value) => {
+    handleMouseOver(value)
+})
 </script>
 
 <style scoped>
+.toggle-button-container {
+    width: 2.6em;
+}
+
 .highlight {
     border: solid var(--main-border-width);
     border-color: var(--primary-color);
@@ -38,10 +56,25 @@ const currentIcon = computed(() => (props.value ? props.icons.checked : props.ic
 
 button {
     pointer-events: all;
+    border-radius: 1.5rem;
 }
 button svg {
     position: relative;
-    top: 0.15em;
-    margin-right: 0.35em;
+    top: 0.1em;
+}
+
+.hover-element {
+    position: relative;
+    bottom: 1.7em;
+    left: -0.5em;
+    height: 0;
+    overflow: visible;
+}
+.hover-element small:first-child {
+    background-color: var(--main-background-color);
+    max-width: 6em;
+    padding: 0.1em 0.6em;
+    padding-bottom: 0.2em;
+    border-radius: calc(var( --main-border-radius) - 0.2em);
 }
 </style>
