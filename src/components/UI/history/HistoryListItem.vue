88f6e528ead13365fab081c2b61bab8fabc0ea5a
pettmatt <petterimattila@protonmail.com>
2024-07-23 13:30:47 +0300

    Creating new components for SessionHistory component

---
+++ src/components/UI/history/HistoryListItem.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="history-list-item flex-horizontal">
+    <h3>{{ new Date(props.item?.date) }}</h3>
+    <div>{{ props.item?.category }}</div>
+    <div>{{ props.item?.session.distance }} km</div>
+    <div>{{ Number(props.item?.session.endingTime) - Number(props.item?.session.startingTime) }} min</div>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { type History } from "@/services/local-storage-service"
+
+const props = defineProps({
+    item: History
+})
+</script>
---
+++ src/components/UI/history/HistorySummary.vue
@@ -0,0 +1,11 @@
+<template>
+<ul>
+    {{ props.historyStatistics ? "Statistics" : "No statistics to show" }}    
+</ul>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    history: Array<History>
+})
+</script>
