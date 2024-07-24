531d45c9f9bd9dbc7a2f4d79b27c4d263348a299
pettmatt <petterimattila@protonmail.com>
2024-07-24 13:16:22 +0300

    Creating progress bar component and implementing it

--- src/components/UI/SessionMenu.vue
+++ src/components/UI/SessionMenu.vue
@@ -1,27 +1,46 @@
 <template>
-<h2>Session menu</h2>
+<h2>Current session</h2>
 
-<p>Progress: {{ distances.current || 0 }} m</p>
-<p v-show="distances.goal">Your goal: {{ distances.goal }} km</p>
+<ProgressBar v-if="distances.goal" :progress="distances.current || 0" :max="distances.goal || 0" />
+<ProgressBar v-if="distances.goal" :progress="timer" :max="time.goalEndingTime" />
 
 <button @click="endSession">End the session</button>
 </template>
 
 <script setup lang="ts">
+import ProgressBar from "./generic/ProgressBar.vue"
 import { addItemToLocalStorage, convertStringValue, getItemFromLocalStorage, type History } from "@/services/local-storage-service"
 import { currentTime } from "@/services/time-service"
 import { hudStore, sessionDetails } from "@/stores/hud-store"
-import { ref } from "vue"
+import { computed, onMounted, onUnmounted, ref } from "vue"
 
 interface DistanceObject {
     current: null | undefined | number,
     goal: null | undefined | number
 }
 
+interface TimeObject {
+    startingTime: number,
+    goalEndingTime: number
+}
+
 const distances = ref<DistanceObject>({
     current: sessionDetails.session.traveledDistance,
     goal: sessionDetails.goal.distance
 })
+
+const time = ref<TimeObject>({
+    startingTime: sessionDetails.session.startingTime || 0,
+    goalEndingTime: sessionDetails.goal.time || 0
+})
+
+const timer = ref<number>(0)
+let intervalId: number | null = null
+
+function increaseTimer() {
+    const updatedTimer = currentTime() - time.value.startingTime
+    timer.value = updatedTimer
+}
 
 function getStatistics(): History {
     return {
@@ -33,12 +52,20 @@ function getStatistics(): History {
 function endSession() {
     hudStore.sessionStartStatus = false
     sessionDetails.session.endingTime = currentTime()
-    const sessionStatistics: History = getStatistics()
+    const sessionStatistics = getStatistics()
     const historyString = getItemFromLocalStorage("history") 
     const history: History[] | [] = convertStringValue(historyString || "[]")
     history.unshift(sessionStatistics)
     addItemToLocalStorage("history", JSON.stringify(history))
 }
+
+onMounted(() => {
+    intervalId = setInterval(increaseTimer, 1000)
+})
+onUnmounted(() => {
+    if (intervalId)
+        clearInterval(intervalId)
+})
 </script>
 
 <style>
---
+++ src/components/UI/generic/ProgressBar.vue
@@ -0,0 +1,72 @@
+<template>
+<div id="progress-bar" class="flex-horizontal">
+    <div class="left-side" :style="`width:${ progressWidthLeft }%;${ handleRightBorderRadius }`">{{ props.progress }}</div>
+    <div class="right-side" :style="`${ progressWidthRight }${ handleLeftBorderRadius }`">{{ props.max }}</div>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    progress: {
+        type: Number,
+        required: true
+    },
+    max: {
+        type: Number,
+        required: true
+    },
+    allowPassingMax: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const progressWidthLeft = computed(() => {
+    return Math.min((props.progress / props.max) * 200)
+})
+const progressWidthRight = computed(() => {
+    if (props.progress / props.max * 100 >= 100)
+        return "width: 0;"
+    return "flex-grow: 1;"
+})
+
+const handleLeftBorderRadius = computed(() => {
+    if (progressWidthLeft.value < 1)
+        return `border-top-left-radius:0.2em;border-bottom-left-radius:0.2em;`
+    else
+        return `border-top-left-radius:0em;border-bottom-left-radius:0em;`
+})
+
+const handleRightBorderRadius = computed(() => {
+    if (progressWidthLeft.value >= 100)
+        return `border-top-right-radius:0.2em;border-bottom-right-radius:0.2em;`
+    else
+        return `border-top-right-radius:0em;border-bottom-right-radius:0em;`
+})
+</script>
+
+<style scoped>
+#progress-bar {
+    gap: 0;
+}
+.left-side,
+.right-side {
+    text-align: center;
+    font-weight: 600;
+}
+.left-side {
+    border-top-left-radius: 0.2em;
+    border-bottom-left-radius: 0.2em;
+    background-color: aliceblue;
+    color: var(--main-background-color-light);
+}
+.right-side {
+    overflow-x: hidden;
+    min-width: 0;
+    border-top-right-radius: 0.2em;
+    border-bottom-right-radius: 0.2em;
+    background-color: var(--main-background-color);
+}
+</style>
