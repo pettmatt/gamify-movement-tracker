fa503f206ad397e1a322b7d478ed8a6d8e051553
pettmatt <petterimattila@protonmail.com>
2024-07-26 12:15:12 +0300

    Removed unused imports

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import TheHud from "./components/TheHud.vue"
 import LeafletMap from "./components/LeafletMap.vue"
 import { hudStore, settingsStore } from "./stores/hud-store"
 import { watch } from "vue"
-import { addItemToLocalStorage, convertStringValue, getItemFromLocalStorage, ifNullUseDefaultValue, synchronizeLocalStorage } from "./services/local-storage-service"
+import { addItemToLocalStorage, synchronizeLocalStorage } from "./services/local-storage-service"
 
 // Handles updating the local storage, so the local storage logic can be centralized in one spot. Individual components should only make changes to the application's store values.
 watch(() => hudStore.historyList, (updatedList) => {
