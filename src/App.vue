f39e4ea8edc22d7db7b587e33a078eafbebde6f0
pettmatt <petterimattila@protonmail.com>
2024-07-26 12:14:53 +0300

    Moved most local storage managments to App component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import "@/assets/main.css"
 import TheHud from "./components/TheHud.vue"
 import LeafletMap from "./components/LeafletMap.vue"
-import "@/assets/main.css"
+import { hudStore, settingsStore } from "./stores/hud-store"
+import { watch } from "vue"
+import { addItemToLocalStorage, convertStringValue, getItemFromLocalStorage, ifNullUseDefaultValue, synchronizeLocalStorage } from "./services/local-storage-service"
+
+// Handles updating the local storage, so the local storage logic can be centralized in one spot. Individual components should only make changes to the application's store values.
+watch(() => hudStore.historyList, (updatedList) => {
+	addItemToLocalStorage("history", JSON.stringify(updatedList))
+})
+watch(() => settingsStore.settings, (updatedSettings) => {
+	synchronizeLocalStorage(updatedSettings)
+}, { deep: true })
 </script>
 
 <template>
